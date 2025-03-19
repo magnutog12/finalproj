a2e9e03f4968c08df38d1e82b91509abd2044bc9
Magnus <magnusterry2@gmail.com>
2025-03-19 14:11:26 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import NavBar from "./navBar";
+import React from 'react';
+import './App.css';
+import data from './data.json';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+  return (
+    <div>
+    <NavBar />
+    <div className="App">
+      
+      {  
+      <div className="leftside">
+        {data.map(data => (
+          <div className="boink">
+            <a href={data.img}>
+            <img src = {data.img} alt='offender' width={130} height={130}></img>
+            </a>
+            <p className="frontpage-name">{data.name}</p>
+            <p>{data.address}</p>
+            <p>{data.locality}</p>
+          </div>
+          
+        ))}
+      </div>
+    }
+        
+   
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/navBar.js
@@ -0,0 +1,122 @@
+//https://stackoverflow.com/questions/15175783/how-can-i-filter-json-when-searching-for-a-sub-part-of-a-string
+//https://react-bootstrap.netlify.app/docs/components/modal/
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Data from './data.json'
+import './App.css';
+import React, { useState } from "react";
+
+// this ended up being navbar and searching funtionallity
+function NavBar() {
+  // use states are react hooks (https://react.dev/reference/react/hooks) used to update a state 
+  // (first parameter) and set state (second parameter) to add states to functional components.
+    const [name, searchName] = useState(""); 
+    const [results, setResults] = useState([]);
+    const [selectedPerson, setSelectedPerson] = useState(null);
+    const [showModal, setModal] = useState(false);
+
+    function search(name) {
+        name = name.toLowerCase();
+        return Data.filter(function(obj) {
+            return obj.name.toLowerCase().includes(name);
+        });
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const result = search(name);
+        setResults(result);
+    };
+
+    const handleShow = (person) => {
+      setSelectedPerson(person);
+      setModal(true);
+    }
+    const handleClose = () => {
+      setModal(false);
+      setSelectedPerson(null);
+    } 
+
+  return (
+    <div>
+    {/*these are react-bootstrap components. They are just prebuilt components that make it easier
+       and they have their own styling*/}
+
+    {/*this is the actual navbar part*/}
+    <Navbar className="bg-body-tertiary justify-content-between navbar-fixed-top">
+      <Button className='navbar-brand' variant='light' href='index.html'>Home</Button>    
+      <Form className="d-flex">
+      
+      </Form>
+      
+      {/*Search functionality // displays filtered group of offenders*/}
+      <Form className="d-flex" onSubmit={handleSubmit}> 
+            
+            <Form.Control
+              type="text"
+              placeholder="Type something..."
+              className=" mr-sm-2"
+
+              value={name}
+              onChange={(e) => searchName(e.target.value)}
+            />
+          
+          <Button type="submit" variant="outline-dark" className="rounded px-4">Search</Button>
+          
+      </Form>
+    </Navbar>
+
+    {/*This is how you inject JS into html // Fixes listing showing up as // &nbsp; for spaces""*/}
+    {name !== "" && <p>&nbsp;&nbsp;&nbsp;Listings for "{name}":</p>}
+   
+    {/*what is displayed on handleSubmit*/}
+    <ul>
+      
+      {results.map((item) => (
+        
+        <li className="gap-2" key={item.name}>
+          
+          {/*handles modal popups*/}
+          <Button className="m-1" variant="outline-dark" size='sm' onClick={() => handleShow(item)}>
+            
+          {item.name}</Button>
+
+        </li>
+        
+      ))}
+      
+    </ul>
+
+    {/*a modal is a overlay/popup, in this case when a button is clicked*/}
+
+    {/*this prevents an error saying selectedPerson is null*/}
+    {/*modal popup when clicked on offenders name in search*/}
+    {selectedPerson && (
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{selectedPerson.name}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>  
+          <div key={selectedPerson.name}>
+            <h1>{selectedPerson.address}</h1>
+            <img src={selectedPerson.img} alt="offender" width={200} height = {200}></img>
+          </div>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="outline-dark" onClick={handleClose}>
+            Close
+          </Button>
+  
+        </Modal.Footer>
+      </Modal>
+    )}
+     
+    </div>
+ 
+  );
+}
+
+export default NavBar;
