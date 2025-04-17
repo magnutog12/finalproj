3aa72749e5e4910e36b1e6af76fac60a3c3e99bf
Magnus <magnusterry2@gmail.com>
2025-04-17 00:12:22 -0400

    markers for offenders working. added modal popup when markers are clicked

--- src/mapUse.js
+++ src/mapUse.js
@@ -1,11 +1,61 @@
 // Map.js
 import React, { useState, useEffect } from 'react';
-import { Marker, Map, InfoWindow, APIProvider } from '@vis.gl/react-google-maps';
+import { Marker, Map, InfoWindow, APIProvider, useMapsLibrary, useMap } from '@vis.gl/react-google-maps';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Data from './data.json';
 
 export default function MapAccess() {
   const [currentLocation, setCurrentLocation] = useState(null);
   const [mPosition, setMPosition] = useState(null);
   const [selectedM, setSelectedM] = useState(null);
+
+  const [geocodingService, setgeocodingService] = useState(null);
+  const [offenderMarker, setOffenderMarker] = useState([]);
+
+  // geocoding addresses!!! ref: https://www.youtube.com/watch?v=cOSw0Vmi3uQ and https://stackoverflow.com/questions/15108316/google-typeerror-google-maps-geocoder-is-not-a-constructor
+  useEffect(() => {
+    // found this .then statement on stack overflow that fixed an issue 
+    window.google.maps.importLibrary("geocoding").then(() => { 
+    if (window.google && window.google.maps) {
+      setgeocodingService(new window.google.maps.Geocoder());
+    }
+  });
+  }, []);
+
+  useEffect(() => {
+    if(!geocodingService) return;
+    const geocodeAddress = async () => {
+
+      const results = []; 
+      for(const offender of Data){
+        // concatenate full address
+        const address = `${offender.address}, ${offender.locality}, TN`;;
+        
+        const res = await geocodingService.geocode({ address });
+        if (res.results && res.results.length > 0) {
+          const location = res.results[0].geometry.location;
+          
+          results.push({
+            name: offender.name,
+            img: offender.img,
+            address: offender.address,
+            locality: offender.locality,
+            lat: location.lat(),
+            lng: location.lng()
+          });
+
+        }
+      }
+      setOffenderMarker(results);
+    };
+    geocodeAddress();
+  
+     //dependencies array
+  }, [geocodingService]);
+
+
+
 
   const mapOptions = {
     draggable: true,
@@ -17,6 +67,7 @@ export default function MapAccess() {
   useEffect(() => {
     const requestLocation = () => {
       if (navigator.geolocation) { // LOOK INTO NAVIGATOR GEOPOSITION
+        
         navigator.geolocation.getCurrentPosition(
           (position) => {
             const location = {
@@ -59,7 +110,9 @@ export default function MapAccess() {
     requestLocation(); // Request location on component mount
   }, []);
 
-  const handleMapClick = (event) => {
+
+
+  /*const handleMapClick = (event) => {
     console.log("Map clicked event:", event);
     // Doesnt currently work in the Map component
     if (event.latLng && typeof event.latLng.lat === "function") {
@@ -73,18 +126,31 @@ export default function MapAccess() {
     } else {
       console.error("Unable to extract coordinates from event:", event);
     }
-  };
+  };*/
   // location if geolocation fails or the current location.
   const defaultCenter = currentLocation || { lat: 35.9653, lng: -83.9233 };
+
+  // modal stuff when a marker is clicked!
+  const [showModal, setModal] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState(null);
+
+  const handleShow = (person) => {
+    setSelectedPerson(person);
+    setModal(true);
+  }
+  const handleClose = () => {
+    setModal(false);
+    setSelectedPerson(null);
+  } 
 
   return (
     <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY} onLoad={() => console.log('Maps API loaded')}>
       <div style={{ height: "100vh", width: "100%" }}>
         <Map 
-            defaultZoom={9} 
+            defaultZoom={15} 
             defaultCenter={defaultCenter} 
             options={mapOptions} 
-            onClick={handleMapClick}
+            //onClick={handleMapClick}
         >
           {mPosition && (
             <Marker 
@@ -92,20 +158,48 @@ export default function MapAccess() {
             onClick={() => setSelectedM(mPosition)}
             />
           )}
-          {selectedM && (
-            <InfoWindow 
-            position={selectedM} 
-            onCloseClick={() => setSelectedM(null)}
-            >
-              <p>
-                {selectedM === currentLocation
-                    ? "Your current location"
-                    : `Marker at lat: ${selectedM.lat.toFixed(4)}, lng: ${selectedM.lng.toFixed(4)}`
-                }
-                </p>
-            </InfoWindow>
-          )}
+          {
+            offenderMarker.map((offender, index) => (
+              <Marker key={index}
+              position={{lat: offender.lat, lng: offender.lng}}
+              icon={{path: window.google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+                scale: 6,
+                fillColor: 'gray',
+                fillOpacity: 0.7,
+                strokeWeight: 0,
+                strokeColor: 'black',}}
+                onClick={() => handleShow(offender)}
+              />
+    
+            ))
+          
+          }
         </Map>
+
+        {selectedPerson && (
+        <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{selectedPerson.name}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>  
+          <div key={selectedPerson.name}>
+            <h1>{selectedPerson.address}, {selectedPerson.locality} TN</h1>
+            <img src={selectedPerson.img} alt="offender" width={200} height = {200}></img>
+            <p></p>
+            <Button size='sm' variant='outline-dark' href={`https://www.truthfinder.com/search/?firstName=${selectedPerson.name.split(' ')[1]}&lastName=${selectedPerson.name.split(' ')[2]}&state=TN&traffic%5Bsource%5D=GOOGSRCH&_gl=1*1cx8y1u*_up*MQ..*_gs*MQ..&gclid=CjwKCAjw47i_BhBTEiwAaJfPpvHnkO14PUegPvUu38lVsr70KfbJAv85JQUJCajUJ5hQtO9a_KGa4BoCAsEQAvD_BwE`}>View Criminal Records</Button>
+          </div>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="outline-dark" onClick={handleClose}>
+            Close
+          </Button>
+  
+        </Modal.Footer>
+      </Modal>
+      )} 
+
       </div>
     </APIProvider>
   );
