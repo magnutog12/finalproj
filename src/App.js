c466434913018f58d01703aa03f1cf02b81857b4
Commwell <cwellvet@gmail.com>
2025-04-22 13:39:34 -0400

    app hosting and debug

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ import data from './data.json';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {createRoot} from "react-dom/client";
 import MapAccess from "./mapUse";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
 //import {APIProvider, Map} from '@vis.gl/react-google-maps';
+
+if (!firebase.apps.length) {
+firebase.initializeApp({
+  apiKey: "AIzaSyBCwjalynLRr1J2C9gqf33aOuPUuN2xgaw",
+  authDomain: "knoxville-watchdog.firebaseapp.com",
+  projectId: "knoxville-watchdog",
+  storageBucket: "knoxville-watchdog.firebasestorage.app",
+  messagingSenderId: "970872123649",
+  appId: "1:970872123649:web:46cb9ba37ee6228f646b00",
+  measurementId: "G-CDLJ0PH3XE"
+});
+}
+
 
 function App() {
 
